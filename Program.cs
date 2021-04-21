97f6f875dabf3ba0224dccfdce495e571869a477
Саня <sanya@Sergejs-MacBook-Air.local>
2021-04-21 13:33:05 +0600

    Созданы переменные row и column - размер массива, создан двумерный массив A[i, j], создан цикл for, который заполняет массив случайными элементами с помощью Random(), сделан вывод массива

--- Program.cs
+++ Program.cs
@@ -6,7 +6,40 @@ namespace Task10v7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //* В матрице А(3-строки,4-столбца) поменять местами наименьшие
+            //* элементы в первой и третей строке.
+
+            int row = 3;
+            int column = 4;
+            int[,] A = new int[row, column];
+            Random random = new Random();
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    A[i, j] = random.Next(100);
+                }
+            }
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    Console.Write($"{A[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+
+            //* Составить программу, которая заполняет квадратную матрицу порядка n 
+            //* натуральными числами 1, 2, 3, .. ., n2,записывая их в нее "по спирали" 
+            //* против часовой стрелки.
+
+
+
+            //* Найти в каждой строке матрицы координаты элемента, равного k(если он 
+            //* там есть). Метод поиска - последовательный с барьером.
         }
     }
 }
